2e43120f304f8c132f8abc8570c44753afd96a5d
Podkopaev Anton <podkoav239@gmail.com>
2019-03-15 20:51:40 +0300

    Proved the WWMM->IMM theorem.

--- src/wwmm/WWMMToimm_s.v
+++ src/wwmm/WWMMToimm_s.v
@@ -85,6 +85,12 @@ Qed.
 Lemma hb_ww_in_hb : hb_ww ⊆ hb.
 Proof. unfold imm_s_hb.hb, WWMM.hb. by rewrite sw_ww_in_sw. Qed.
 
+Lemma hb_wwE : hb_ww ≡ <|E|> ;; hb_ww ;; <|E|>.
+Proof.
+  apply dom_helper_3. rewrite hb_ww_in_hb.
+  apply dom_helper_3. by apply wf_hbE.
+Qed.
+
 Lemma hb_ww_co_fr_ac :
   acyclic (hb_ww ∪ ⦗ Sc ⦘ ⨾ (fr ∪ co) ⨾ ⦗ Sc ⦘).
 Proof.
@@ -99,16 +105,29 @@ Proof.
 (* acyclic     ([SC]; scb; [SC]) *)
 Admitted.
 
-Lemma s_imm_consistent_implies_wwmm_consistent sc
+Theorem s_imm_consistent_implies_wwmm_consistent sc
       (IPC : imm_s.imm_psc_consistent G sc) :
   exists mo, wwmm_consistent G mo.
 Proof.
   cdes IPC. cdes IC.
-  exists (tot_ext (acts G)
-                  (hb_ww ∪ ⦗ Sc ⦘ ⨾ (fr ∪ co) ⨾ ⦗ Sc ⦘)).
+  exists (<|E|> ;;
+            tot_ext (acts G)
+            (hb_ww ∪ ⦗ Sc ⦘ ⨾ (fr ∪ co) ⨾ ⦗ Sc ⦘) ;;
+          <|E|>).
   red. splits.
-  { apply tot_ext_total. }
-  { rewrite <- tot_ext_extends. eauto with hahn. }
+  { red. ins.
+    edestruct tot_ext_total.
+    3: by eauto.
+    all: eauto.
+    { left.
+      apply seq_eqv_l. split; auto.
+      apply seq_eqv_r. split; eauto. }
+    right.
+    apply seq_eqv_l. split; auto.
+    apply seq_eqv_r. split; eauto. }
+  { rewrite <- tot_ext_extends.
+    rewrite hb_wwE at 1.
+    hahn_frame. eauto with hahn. }
   { rewrite hb_ww_in_hb, rf_in_eco.
     arewrite (eco ⊆ eco^?). apply IPC. }
   { unfolder. intros w' [r [HBWR HH]].
@@ -119,8 +138,8 @@ Proof.
     apply seq_eqv_r in HB. destruct HB as [HB EW'].
     apply (dom_l WF.(wf_rfD)) in RF.
     apply seq_eqv_l in RF. destruct RF as [WW RF].
-    assert (exists l, loc w = Some l) as [l LL].
-    { admit. }
+    edestruct is_w_loc as [l LL].
+    { apply WW. }
     assert (w <> w') as NEQ.
     { intros HH. subst.
       eapply hb_irr; eauto. }
@@ -133,13 +152,100 @@ Proof.
     eapply Cint. exists w'. split; eauto.
     right. by apply co_in_eco. }
   { intros w' HH.
-    apply seq_eqv_l in HH. destruct HH as [WW' HH].
+    apply seq_eqv_l in HH. destruct HH as [[WW' SCW'] HH].
     destruct HH as [r [MO HH]].
-    apply seq_eqv_l in HH. destruct HH as [RR HH].
+    apply seq_eqv_l in HH. destruct HH as [[RR SCR] HH].
     destruct HH as [w [RF HH]].
-    apply seq_eqv_l in HH. destruct HH as [WW [MOWW SL]].
-    admit. }
-  1-2: admit.
-Admitted.
+    apply seq_eqv_l in HH. destruct HH as [[WW SCW] [MOWW SL]].
+    apply seq_eqv_l in MOWW. destruct MOWW as [EW' MOWW].
+    apply seq_eqv_r in MOWW. destruct MOWW as [MOWW EW].
+    edestruct is_w_loc as [l LL].
+    { apply WW. }
+    assert (w <> w') as NEQ.
+    { intros HH. subst.
+      eapply tot_ext_irr; [|by eauto].
+      apply hb_ww_co_fr_ac. }
+    edestruct WF.(wf_co_total).
+    3: by eauto.
+    1,2: by unfolder; splits.
+    all: eapply tot_ext_irr;
+         [by apply hb_ww_co_fr_ac|].
+    { eapply tot_ext_trans.
+      { generalize MO. basic_solver. }
+      apply tot_ext_extends.
+      right.
+      apply seq_eqv_l. split; [by apply SCR|].
+      apply seq_eqv_r. split; [|by apply SCW'].
+      left.
+      eexists. eauto. }
+    eapply tot_ext_trans; eauto.
+    apply tot_ext_extends.
+    right.
+    apply seq_eqv_l. split; auto.
+    apply seq_eqv_r. split; auto.
+      by right. }
+  { intros w' HH.
+    apply seq_eqv_l in HH. destruct HH as [[WW' SCW'] HH].
+    destruct HH as [r [HBWR' HH]].
+    destruct HH as [w [[HBWR RF] HH]].
+    apply seq_eqv_l in HH. destruct HH as [[WW SCW] [MOWW SL]].
+    apply seq_eqv_l in MOWW. destruct MOWW as [EW' MOWW].
+    apply seq_eqv_r in MOWW. destruct MOWW as [MOWW EW].
+    edestruct is_w_loc as [l LL].
+    { apply WW. }
+    assert (w <> w') as NEQ.
+    { intros HH. subst.
+      eapply tot_ext_irr; [|by eauto].
+      apply hb_ww_co_fr_ac. }
+    edestruct WF.(wf_co_total).
+    3: by eauto.
+    1,2: by unfolder; splits.
+    { eapply Cint.
+      eexists. split.
+      { apply hb_ww_in_hb. apply HBWR'. }
+      right. apply fr_in_eco.
+      eexists. eauto. }
+    eapply tot_ext_irr; [by apply hb_ww_co_fr_ac|].
+    eapply tot_ext_trans; eauto.
+    apply tot_ext_extends.
+    right.
+    apply seq_eqv_l. split; auto.
+    apply seq_eqv_r. split; auto.
+      by right. }
+  intros w' HH.
+  apply seq_eqv_l in HH. destruct HH as [[WW' SCW'] HH].
+  destruct HH as [r [MO HH]].
+  apply seq_eqv_l in MO. destruct MO as [EW' MO].
+  apply seq_eqv_r in MO. destruct MO as [MO ER].
+  apply seq_eqv_l in HH. destruct HH as [[RR SCR] HH].
+  destruct HH as [w [[HBWR RF] [HBWW SL]]].
+  apply (dom_l WF.(wf_rfD)) in RF.
+  apply seq_eqv_l in RF. destruct RF as [WW RF].
+  apply (dom_l WF.(wf_rfE)) in RF.
+  apply seq_eqv_l in RF. destruct RF as [EW RF].
+  edestruct is_w_loc as [l LL].
+  { apply WW. }
+  assert (w <> w') as NEQ.
+  { intros HH. subst.
+    eapply hb_irr; eauto.
+    apply hb_ww_in_hb. eauto. }
+  edestruct WF.(wf_co_total).
+  3: by eauto.
+  1,2: by unfolder; splits.
+  { eapply tot_ext_irr; [by apply hb_ww_co_fr_ac|].
+    eapply tot_ext_trans.
+    { generalize MO. basic_solver. }
+    apply tot_ext_extends.
+    right.
+    apply seq_eqv_l. split; [by apply SCR|].
+    apply seq_eqv_r. split; [|by apply SCW'].
+    left.
+    eexists. eauto. }
+  eapply Cint.
+  eexists.
+  split.
+  { apply hb_ww_in_hb. apply HBWW. }
+  right. by apply co_in_eco.
+Qed.
 
 End WWMM_TO_IMM_S.
