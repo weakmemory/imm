d5b19eba56ddd6c164c2f680e6b197357c1ec4aa
Podkopaev Anton <podkoav239@gmail.com>
2019-04-11 16:14:11 +0300

    working on co_sc_in_hb

--- src/ocamlmm/OCamlToimm_s.v
+++ src/ocamlmm/OCamlToimm_s.v
@@ -71,14 +71,124 @@ Notation "'Loc_' l" := (fun x => loc x = Some l) (at level 1).
 Notation "'Init'" := (fun a => is_true (is_init a)).
 
 Hypothesis LSM : forall l,
-    << LM : Loc_ l \₁ Init  ⊆₁ Rlx >> \/
+    << LM : Loc_ l \₁ Init  ⊆₁ Rlx \₁ Sc >> \/
     << LM : Loc_ l \₁ Init  ⊆₁ Sc >>.
-Hypothesis WSCRMW : W∩₁Sc ⊆₁ codom_rel (<|R∩₁Sc|> ;; rmw).
+
+Hypothesis WSCRMW : W∩₁Sc ≡₁ codom_rel rmw.
+Hypothesis RMWSC  : rmw ≡ ⦗Sc⦘ ⨾ rmw ⨾ ⦗Sc⦘.
+
 Hypothesis WRLXF : W∩₁Rlx ⊆₁ codom_rel (<|F∩₁Acq/Rel|> ;; immediate sb).
 Hypothesis RSCF : R∩₁Sc ⊆₁ codom_rel (<|F∩₁Acq|> ;; immediate sb).
 
-Lemma co_sc_in_hb (WF : Wf G) : <|Sc|> ;; co ;; <|Sc|> ⊆ hb.
+Lemma co_sc_in_hb (WF : Wf G) sc
+      (IPC : imm_s.imm_psc_consistent G sc) :
+  <|Sc|> ;; co ;; <|Sc|> ⊆ hb.
 Proof.
+  rewrite fsupp_imm_t with (r:=⦗Sc⦘ ⨾ co ⨾ ⦗Sc⦘).
+  4: { generalize WF.(co_trans). basic_solver. }
+  3: { generalize WF.(co_irr). basic_solver. }
+  2: { arewrite (⦗Sc⦘ ⨾ co ⨾ ⦗Sc⦘ ⊆ co) by basic_solver.
+       rewrite WF.(wf_coE). unfold acts_set.
+       red. ins. eexists. basic_solver. }
+  
+  assert (transitive hb) as THB by apply hb_trans.
+  apply inclusion_t_ind; auto.
+  arewrite (immediate (⦗Sc⦘ ⨾ co ⨾ ⦗Sc⦘) ⊆ ⦗Sc⦘ ⨾ rf ⨾ ⦗Sc⦘ ⨾ sb).
+  2: { arewrite (⦗Sc⦘ ⨾ rf ⨾ ⦗Sc⦘ ⊆ sw).
+       2: { rewrite sw_in_hb, sb_in_hb.
+            apply rewrite_trans; auto. }
+       arewrite (Sc ⊆₁ Rel) at 1 by mode_solver.
+       arewrite (Sc ⊆₁ Acq)      by mode_solver.
+       unfold imm_s_hb.sw, imm_s_hb.release, imm_s_hb.rs.
+       rewrite !seqA.
+       hahn_frame.
+       rewrite (dom_l WF.(wf_rfD)) at 1.
+       basic_solver 40. }
+  rewrite (dom_r WF.(wf_coD)).
+  rewrite !seqA.
+  rewrite <- id_inter.
+  rewrite WSCRMW.
+  intros x y [HH AA].
+  apply seq_eqv_l in HH. destruct HH as [SCX HH].
+  apply seq_eqv_r in HH. destruct HH as [HH RMWY].
+  destruct RMWY as [z ZY].
+  apply seq_eqv_l. split; auto.
+  assert (Sc z) as SCZ.
+  { apply RMWSC in ZY. generalize ZY. basic_solver. }
+
+  assert (E z) as EZ.
+  { apply (dom_l WF.(wf_rmwE)) in ZY.
+    generalize ZY. basic_solver. }
+  assert (R z) as EW.
+  { apply (dom_l WF.(wf_rmwD)) in ZY.
+    generalize ZY. type_solver. }
+
+  exists z. split.
+  2: { apply seq_eqv_l. split; auto.
+       apply rmw_in_sb; auto. }
+  assert (exists v, rf v z) as [v RF].
+  { apply IPC. split; auto. }
+  destruct (classic (x = v)) as [|NEQ]; desf.
+
+  assert (E v) as EV. 
+  { apply WF.(wf_rfE) in RF. generalize RF. basic_solver. }
+  assert (W v) as WV. 
+  { apply WF.(wf_rfD) in RF. generalize RF. basic_solver. }
+  
+  set (GG := WV).
+  apply is_w_loc in GG. desf.
+  
+  assert (loc z = Some l) as LOCZ.
+  { rewrite <- GG. symmetry.
+    apply wf_rfl; auto. }
+  
+  destruct (classic (Init v)) as [INIT|NINIT].
+  { admit. }
+  assert (Sc v) as SCV.
+  { specialize (LSM l).
+    destruct LSM as [CC|CC].
+    2: { apply CC. split; auto. }
+    exfalso.
+    assert (~ is_init z) as NINITZ.
+    { eapply read_or_fence_is_not_init; eauto. }
+    assert ((Loc_ l \₁ Init) z) as DD.
+    { split; auto. }
+   apply CC in DD.
+   destruct DD. desf. }
+
+  assert (codom_rel rmw v) as RMWV.
+  { apply WSCRMW. split; auto. }
+
+  assert (E x) as EX.
+  { apply (dom_l WF.(wf_coE)) in HH.
+    generalize HH. basic_solver. }
+  assert (W x) as WX.
+  { apply (dom_l WF.(wf_coD)) in HH.
+    generalize HH. basic_solver. }
+
+  eapply wf_co_total in NEQ; eauto.
+  3: { split; [split|]; auto. }
+  2: { split; [split|]; auto.
+       rewrite GG, <- LOCZ.
+       arewrite (loc z = loc y) by (by apply WF.(wf_rmwl)).
+       apply WF.(wf_col); auto. }
+
+  cdes IPC. cdes IC.
+  assert (sc_per_loc G) as SCPL by (by apply coherence_sc_per_loc).
+  
+  exfalso.
+  destruct NEQ as [NEQ|NEQ].
+  2: { eapply atomicity_alt; eauto.
+       split; eauto.
+       do 2 (eexists; split; eauto). }
+  apply AA with (c:=v).
+  { apply seq_eqv_l. split; auto.
+    apply seq_eqv_r. split; auto. }
+  apply seq_eqv_l. split; auto.
+  apply seq_eqv_r. split; auto.
+  2: { eexists; eauto. }
+  apply rf_rmw_in_co; auto.
+  eexists. eauto.
 Admitted.
 
 Lemma s_imm_consistentimplies_ocaml_consistent (WF: Wf G) sc
