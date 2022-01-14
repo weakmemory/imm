4b27d3ccc65ee4b1185240069f31c007a7e1c5d6
Anton Podkopaev <podkoav239@gmail.com>
2022-01-14 19:02:37 +0300

    Proven auxiliary lemmas about fsupp

--- src/traversal/TraversalOrder.v
+++ src/traversal/TraversalOrder.v
@@ -17,6 +17,52 @@ Require Import AuxDef.
 
 Set Implicit Arguments.
 
+(* TODO: move to AuxRel2.v *)
+Require Import IndefiniteDescription.
+Lemma fsupp_rt_ct {A} (r : relation A)
+      (FSUPP : fsupp r＊) :
+  fsupp r⁺.
+Proof using.
+  intros y. specialize (FSUPP y).
+  desf. exists findom.
+  ins. apply FSUPP.
+  apply rtE. now right.
+Qed.
+
+(* TODO: move to AuxRel2.v *)
+Lemma fsupp_wf_implies_fsupp_rt {A} (r : relation A)
+      (WF    : well_founded r)
+      (FSUPP : fsupp r) : 
+  fsupp r⁺.
+Proof using.
+  apply fsupp_rt_ct.
+  red; intros y.
+  induction y as [y HH] using (well_founded_ind WF).
+  specialize (FSUPP y). desf.
+  set (f :=
+         fun (a : A) =>
+           match
+             excluded_middle_informative (r a y)
+           with
+           | left  PF =>
+               proj1_sig (constructive_indefinite_description _ (HH a PF))
+           | right _  => nil
+           end
+      ).
+  exists (y :: flat_map f findom).
+  ins. apply rtE in REL. red in REL. desf.
+  { red in REL. desf. auto. }
+  right.
+  apply ct_end in REL.
+  destruct REL as [z [AA BB]].
+  apply in_flat_map.
+  exists z. splits.
+  { now apply FSUPP. }
+  subst f. ins. unfold proj1_sig. desf.
+  intuition.
+Qed.
+
+
 Module TravAction.
   Inductive t := cover | issue.
 
